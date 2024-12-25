83110e2b9dc3a952860a6208545f99b8a5ed64df
Taha Juzar <167352424+TahaJuzar@users.noreply.github.com>
2024-12-25 21:33:15 +0500

    Update pingpong.py
    It is an updated version of ping pong game, that one paddle is used by AI and other paddle used by human with w button to move up and s button to move down.

--- pingpong.py
+++ pingpong.py
@@ -1,249 +1,132 @@
-print('k')
-import turtle  #Sets up the game window using the turtle graphics library
+import turtle
 import random
 import numpy as np
+
+# Setup the game window
 window = turtle.Screen()
+window.title("Ping Pong Game with AI")
 window.setup(width=800, height=600)
 window.tracer(0)
-class aipaddleright(turtle.Turtle): #this will make right side paddle that is controlled by AI
-    def __init__(self,*args,**kwargs):
-        super(aipaddleright,self).__init__(*args,**kwargs)
-        self.speed(0)
-        self.shape("square")
-        self.color("green")
-        self.shapesize(stretch_wid=5, stretch_len=1) #it will create a rectangular paddle
+
+# AI Paddle Class
+class aipaddleright(turtle.Turtle):
+    def _init_(self, *args, **kwargs):
+        super(aipaddleright, self)._init_(*args, **kwargs)
+        self.speed(0)  # Paddle speed
+        self.shape("square")  # Paddle shape
+        self.color("green")  # Paddle green color
+        self.shapesize(stretch_wid=5, stretch_len=1)
         self.penup()
-        self.goto(350,0) #defalut place starting
-        self.offlineq =False
-        self.fitness=0
-        self.speed(speed=0)
-        #my neral networkk
-        self.array1as = 2*np.random.random((5,7))-1
-        self.array3as = 2*np.random.random((7,3))-1
-    def dissappear(self):
-        self.hideturtle()
-        self.offlineq=True
-    def usebraintomove(self,ballpos): #ballpos will give [[xspeed,yspeed],[x2,y2]]
-        inputlayer = np.array([ballpos[0][0]*1.5,ballpos[0][1]*1.5,ballpos[1][0]/400,ballpos[1][1]/300,self.ycor()/300])
-        after1stlayer = self.sigmoid(np.dot(inputlayer,self.array1as))
-        finalresults = np.dot(after1stlayer,self.array3as)
-        finalresults = self.softmax(finalresults)
+        self.goto(350, 0)
+        self.array1as = 2 * np.random.random((5, 7)) - 1
+        self.array3as = 2 * np.random.random((7, 3)) - 1
+
+    def usebraintomove(self, ballpos):
+        inputlayer = np.array([ballpos[0][0] * 1.5, ballpos[0][1] * 1.5, ballpos[1][0] / 400, ballpos[1][1] / 300, self.ycor() / 300])
+        after1stlayer = self.sigmoid(np.dot(inputlayer, self.array1as))
+        finalresults = self.softmax(np.dot(after1stlayer, self.array3as))
         return finalresults
 
-    def sigmoid(self,x):
+    def move_ai(self, ballpos):
+        probs = self.usebraintomove(ballpos)
+        move_decision = random.uniform(0, 1)
+        if move_decision <= probs[0]:  # Move up
+            self.sety(min(self.ycor() + 10, 250))
+        elif move_decision <= probs[0] + probs[1]:  # Move down
+            self.sety(max(self.ycor() - 10, -250))
 
-        return np.maximum(0,x)
+    @staticmethod
+    def sigmoid(x):
+        return np.maximum(0, x)
 
-
-    def softmax(self,x):
+    @staticmethod
+    def softmax(x):
         return np.exp(x) / np.sum(np.exp(x), axis=0)
 
-
-    def upkey(self):
-        y=self.ycor()
-        y=y+10
-        self.sety(y)
-        if self.ycor()>300:
-            self.sety(y-10)
-
-    def downkey(self):
-        y=self.ycor()
-        y=y-10
-        self.sety(y)
-        if self.ycor()<-300:
-            self.sety(y+10)
-
-#robott paddle
+# Left paddle controlled by the player
 leftpaddle = turtle.Turtle()
 leftpaddle.speed(0)
 leftpaddle.shape("square")
-leftpaddle.color("green")
-leftpaddle.shapesize(stretch_wid=200, stretch_len=1)
+leftpaddle.color("blue")
+leftpaddle.shapesize(stretch_wid=5, stretch_len=1)
 leftpaddle.penup()
-leftpaddle.goto(-365,0)
-#funcitons to move this
-def godownleft():
-    leftpaddle.sety(leftpaddle.ycor()-40)
-def goupleft():
-    leftpaddle.sety(leftpaddle.ycor()+40)
+leftpaddle.goto(-350, 0)
 
-#the ball
-ball = turtle.Turtle() #it will represent the ball in the game
+def godownleft():
+    y = leftpaddle.ycor() - 20
+    if y > -250:
+        leftpaddle.sety(y)
+
+def goupleft():
+    y = leftpaddle.ycor() + 20
+    if y < 250:
+        leftpaddle.sety(y)
+
+# Ball setup
+ball = turtle.Turtle()
 ball.speed(0)
 ball.shape("circle")
-ball.color("red") #identifies the red color of the ball
+ball.color("red")
 ball.penup()
-ball.goto(200,0)
-ballxdirection=random.uniform(-0.6,-1.4)
-if (random.randint(0,10000) % 2):
-    ballydirection=random.uniform(-0.6,-1.4)
-else:
-    ballydirection=random.uniform(0.6,1.4)
+ball.goto(0, 0)
+ballxdirection = random.uniform(-1.5, -0.5)
+ballydirection = random.uniform(-1, 1)
 
+# AI Paddle setup
+ai_paddle = aipaddleright()
 
-#generate ais
-aidictionary = {}
-neuralnetworksforeach = {}
-for ai in range(100):
-    aidictionary[ai]=aipaddleright()
-
-
-aidictionary[0].upkey()
-aidictionary[2].downkey()
-#scores
+# Scores and Pen
 player_1_score = 0
 player_2_score = 0
-#Pen for the scores
-pen = turtle.Turtle() #it is for scoring of the player and it will update the score after passing to the paddle
+pen = turtle.Turtle()
 pen.speed(0)
 pen.color("black")
 pen.penup()
 pen.hideturtle()
-pen.goto(0,260)
-pen.write("Player 1: 0 Player 2: 0, largest fitness is: 0", align = "center", font = ("Courier", 24, "normal"))
+pen.goto(0, 260)
+pen.write("Player 1: 0 Player 2: 0", align="center", font=("Courier", 24, "normal"))
 
-
-savedthisturnkilled = []
-
-def findclosestperson(listofpeople,ballycor):
-    closestpy = 100000
-    closestperson = None
-    for person in listofpeople:
-        if np.absolute(ballycor-person.ycor())<closestpy:
-            closestpy = np.absolute(ballycor-person.ycor())
-            closestperson = person
-
-    return closestperson
-
-def ontonextround():
-    print('ss')
-
-multiplyratio = 7
-
+# Key bindings
 window.listen()
-window.onkey(goupleft,'w')
-window.onkey(godownleft,'s')
-while True: #main game loop
-    #move ball
+window.onkeypress(goupleft, 'w')  # Move up paddle to use w button
+window.onkeypress(godownleft, 's')  # Move down paddle to use s button
 
-    ball.sety(multiplyratio*ballydirection+ball.ycor()) #it will move the ball based on its speed and direction
-    ball.setx(multiplyratio*ballxdirection+ball.xcor())
-    #hit top or bottom walls THIS SECTION IS DEALING WITH WHERE THE BALL HITS, LIKE LOSING OR BOUNCING
-    if ball.ycor() > 290 or ball.ycor()<-290:
-        ballydirection = 0-ballydirection
-    #hit left me paddle, not ai
-    if ((ball.ycor() > ((leftpaddle.ycor()-500)) and (ball.ycor() < (leftpaddle.ycor()+500)))  and  (ball.xcor()>(leftpaddle.xcor()-10) and (ball.xcor()<leftpaddle.xcor()+10))):
-        ballxdirection = 0-ballxdirection
-    #hit ai paddles loop
-    savedxdirection = ballxdirection
-    needtokill = []
-    for playerkey in aidictionary:
-
-        #actually moving
-        probs = aidictionary[playerkey].usebraintomove([[ballxdirection,ballydirection],[ball.xcor(),ball.ycor()]])
-        whatweget = random.uniform(0,1)
-        if whatweget>=0 and whatweget<=probs[0]:
-            aidictionary[playerkey].upkey()
-        if whatweget>probs[0] and whatweget<=probs[1]:
-            aidictionary[playerkey].downkey()
-
-        #bouncing off paddles
-        if ((ball.ycor() > ((aidictionary[playerkey].ycor()-50)) and (ball.ycor() < (aidictionary[playerkey].ycor()+50)))  and  (ball.xcor()>(aidictionary[playerkey].xcor()-10) and (ball.xcor()<aidictionary[playerkey].xcor()+10))):
-            if ballxdirection == savedxdirection:
-                if aidictionary[playerkey].offlineq == False:
-                    ballxdirection = 0-ballxdirection
-            aidictionary[playerkey].fitness += 1
-            if aidictionary[playerkey].fitness >= 50: # if running for way to long
-                needtokill.append(playerkey)
-        else:
-            if ball.xcor() >= 340:
-                needtokill.append(playerkey)
-
-
-    for kills in needtokill:
-        savedthisturnkilled.append(aidictionary[kills])
-        aidictionary[kills].dissappear()
-
-
-        del aidictionary[kills]
-        if len(aidictionary) == 0: #if everyone dies
-            #leaderboard
-            player_1_score = player_1_score+1
-            pen.clear()
-            pen.write("Player 1: {} Player 2: {}".format(player_1_score, player_2_score), align = "center", font = ("Courier", 24, "normal"))
-
-            nearestpersontotarget = findclosestperson(savedthisturnkilled,ball.ycor())
-            hisbrainsaved = [nearestpersontotarget.array1as,nearestpersontotarget.array3as]
-            fitnessthis = nearestpersontotarget.fitness
-
-            multiplyratio += 0
-
-            for ai in range(150):#regenerate players
-                aidictionary[ai]=aipaddleright()
-                #mutatetion
-                mutateRa=0.18 #mutation rate change to change
-
-                if ai < 100:
-                    mutaterRa = 0.08
-                if ai < 25:
-                    mutateRa=0.04 #mutation rate change to change
-
-                if ai == 0:
-                    mutateRa=0 #mutation rate change to change
-                mutatefir = mutateRa*np.random.random((5,7))-mutateRa/2 #mutateing for new brains
-                mutatemid = mutateRa*np.random.random((7,3))-mutateRa/2 #FIX FOR LATER -- when randomly mutate, only select
-                #apply and run
-                aidictionary[ai].array1as = np.add(hisbrainsaved[0],mutatefir)
-                aidictionary[ai].array3as = np.add(hisbrainsaved[1],mutatemid)
-            print(str(hisbrainsaved))
-            ball.sety(0) #reset balls
-            ball.setx(0)
-            ballxdirection=random.uniform(-0.6,-1.4) #reset ball directions
-            if (random.randint(0,10000) % 2):
-                ballydirection=random.uniform(-0.6,-1.4)
-            else:
-                ballydirection=random.uniform(0.6,1.4)
-
-            print('i lost but my fitness was: '+ str(fitnessthis))
-
-
-    savedthisturnkilled = []
-
-
-
-    #hit left side someone loses
-    if (ball.xcor() < -399):
-        ball.sety(0)
-        ball.setx(0)
-        player_2_score = player_2_score+1
-        pen.clear()
-        pen.write("Player 1: {} Player 2: {}".format(player_1_score, player_2_score), align = "center", font = ("Courier", 24, "normal"))
-        ballxdirection=random.uniform(-0.6,-1.4) #reset ball directions
-        if (random.randint(0,10000) % 2):
-            ballydirection=random.uniform(-0.6,-1.4)
-        else:
-            ballydirection=random.uniform(0.6,1.4)
-    #hit right side someone loses
-    if (ball.xcor() > 350): #AI LOSE
-        ball.sety(0)
-        ball.setx(0)
-        player_1_score = player_1_score+1
-        pen.clear()
-        pen.write("Player 1: {} Player 2: {}".format(player_1_score, player_2_score), align = "center", font = ("Courier", 24, "normal"))
-        ballxdirection=random.uniform(-0.6,-1.4) #reset ball direction so it is fun
-        if (random.randint(0,10000) % 2):
-            ballydirection=random.uniform(-0.6,-1.4)
-        else:
-            ballydirection=random.uniform(0.6,1.4)
-
-
-    #let players move
-    #left paddle robot just follow
-    #if ball.ycor() > leftpaddle.ycor():
-    #    goupleft()
-    #if ball.ycor() < leftpaddle.ycor():
-    #    godownleft()
-
-    #last ball where
+# Game Loop
+while True:
     window.update()
-window.mainloop()
+    ball.setx(ball.xcor() + ballxdirection * 2)  # Reduced from 5 to 2 ball speed
+    ball.sety(ball.ycor() + ballydirection * 2)  # Reduced from 5 to 2 ball speed
+
+    # Ball collision with top and bottom walls
+    if ball.ycor() > 290 or ball.ycor() < -290:
+        ballydirection *= -1
+
+    # Ball collision with left paddle
+    if (-360 < ball.xcor() < -340 and
+            leftpaddle.ycor() - 50 < ball.ycor() < leftpaddle.ycor() + 50):
+        ballxdirection *= -1
+
+    # Ball collision with AI paddle
+    if (340 < ball.xcor() < 360 and
+            ai_paddle.ycor() - 50 < ball.ycor() < ai_paddle.ycor() + 50):
+        ballxdirection *= -1
+
+    # AI Paddle Movement
+    ai_paddle.move_ai([[ballxdirection, ballydirection], [ball.xcor(), ball.ycor()]])
+
+    # Ball out of bounds - scoring
+    if ball.xcor() > 390:
+        player_1_score += 1
+        ball.goto(0, 0)
+        ballxdirection = random.uniform(-1.5, -0.5)
+        ballydirection = random.uniform(-1, 1)
+
+    if ball.xcor() < -390:
+        player_2_score += 1
+        ball.goto(0, 0)
+        ballxdirection = random.uniform(0.5, 1.5)
+        ballydirection = random.uniform(-1, 1)
+
+    # Update scores
+    pen.clear()
+    pen.write(f"Player 1: {player_1_score} Player 2: {player_2_score}", align="center", font=("Courier", 24, "normal"))
